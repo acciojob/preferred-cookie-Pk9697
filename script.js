d8af7c025694519aa3a43f3f68d25cb6c63c7d92
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-28 17:33:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const submitInput=document.querySelector('input[type="submit"]')
+const fontsizeInput=document.getElementById('fontsize')
+const fontcolorInput=document.getElementById('fontcolor')
+const rootElement=document.querySelector(':root')
+
+submitInput.addEventListener('click',(e)=>{
+	e.preventDefault()
+	rootElement.style.setProperty('--fontsize',fontsizeInput.value)
+	rootElement.style.setProperty('--fontcolor',fontcolorInput.value)
+})
