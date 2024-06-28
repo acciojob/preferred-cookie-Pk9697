2474ae0196170ee120021dc2e2c58802d0cca0ab
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-28 18:20:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,30 @@ const submitInput=document.querySelector('input[type="submit"]')
 const fontsizeInput=document.getElementById('fontsize')
 const fontcolorInput=document.getElementById('fontcolor')
 const rootElement=document.querySelector(':root')
-
+ 
+if(document.cookie){
+	// console.log(document.cookie.preferences)
+	const cookie=document.cookie.split('; ').find((cookie)=>cookie.startsWith('preferences'))
+	if(cookie){
+		const preferences=JSON.parse(cookie.split('=')[1])
+		// console.log(JSON.parse(preferences)) 
+		setRootElementProperty(preferences)
+	}
+}
+           
 submitInput.addEventListener('click',(e)=>{
 	e.preventDefault()
-	rootElement.style.setProperty('--fontsize',fontsizeInput.value)
-	rootElement.style.setProperty('--fontcolor',fontcolorInput.value)
+	const preferences={
+		fontsize:fontsizeInput.value,
+		fontcolor:fontcolorInput.value
+	}
+	document.cookie=`preferences=${JSON.stringify(preferences)};`
+	setRootElementProperty(preferences)
 })
+  
+function setRootElementProperty({fontsize,fontcolor}){
+	rootElement.style.setProperty('--fontsize',fontsize)
+	rootElement.style.setProperty('--fontcolor',fontcolor)
+	fontsizeInput.value=fontsize
+	fontcolorInput.value=fontcolor
+}
